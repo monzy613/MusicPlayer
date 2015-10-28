e74c9f84d38dd9fbbfb0d9fb3e0565f4c1680e01
monzy613 <monzy613@gmail.com>
2015-10-28 16:57:41 +0800

    no message

---
+++ MusicPlayer/MP3Player.swift
@@ -0,0 +1,120 @@
+//
+//  MP3Player.swift
+//  MusicPlayer
+//
+//  Created by Monzy on 15/10/28.
+//  Copyright © 2015年 Monzy. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class MP3Player: NSObject, AVAudioPlayerDelegate {
+    var player: AVAudioPlayer?
+    var currentTrackIndex = 0
+    var tracks = [String]()
+    
+    override init() {
+        tracks = FileReader.readFiles()
+        print(tracks)
+        super.init()
+        queueTrack()
+    }
+    
+    func queueTrack() {
+        if player != nil {
+            player = nil
+        }
+        
+        let url = NSURL.fileURLWithPath(tracks[currentTrackIndex])
+        do {
+            self.player = try AVAudioPlayer(contentsOfURL: url)
+        }
+        catch {
+            print("fatal error: \(error)")
+            return
+        }
+        
+        player?.delegate = self
+        player?.prepareToPlay()
+    }
+    
+    func playOrPause(play: Bool) {
+        if player == nil {
+            return
+        }
+        
+        if play {
+            player?.play()
+        } else {
+            player?.pause()
+        }
+        
+    }
+    
+    func next() {
+        if currentTrackIndex == (tracks.count - 1) {
+            currentTrackIndex = 0
+        } else {
+            currentTrackIndex += 1
+        }
+        queueTrack()
+        player?.play()
+    }
+    
+    func previous() {
+        if currentTrackIndex == 0 {
+            currentTrackIndex = tracks.count - 1
+        } else {
+            currentTrackIndex -= 1
+        }
+        queueTrack()
+        player?.play()
+    }
+    
+    func returnWithSeconds(seconds: Float) {
+        if getCurrentTime() > seconds {
+            print("print at time \(getCurrentTime() - seconds)")
+            player?.currentTime = Double(getCurrentTime() - seconds)
+        } else {
+            player?.currentTime = 0
+        }
+        player?.play()
+    }
+    
+    func forwardWithSeconds(seconds: Float) {
+        if getCurrentTime() + seconds < getDuration() {
+            print("print at time \(getCurrentTime() + seconds)")
+            player?.currentTime = Double(getCurrentTime() + seconds)
+        } else {
+            next()
+        }
+        player?.play()
+    }
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        next()
+    }
+    
+    func setProgress(newProgress: Float) {
+        player?.currentTime = Double(getDuration() * newProgress)
+        player?.play()
+    }
+    
+    func getProgress() -> Float {
+        if player != nil {
+            return Float((player?.currentTime)!) / Float((player?.duration)!)
+        } else {
+            return -1.0
+        }
+    }
+    
+    func getDuration() -> Float {
+        return Float((player?.duration)!)
+    }
+    
+    func getCurrentTime() -> Float {
+        return Float((player?.currentTime)!)
+    }
+
+}
---
+++ MusicPlayer/PlayerViewController.swift
@@ -0,0 +1,130 @@
+//
+//  PlayerViewController.swift
+//  MusicPlayer
+//
+//  Created by Monzy on 15/10/28.
+//  Copyright © 2015年 Monzy. All rights reserved.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+    
+    
+    var timer: NSTimer?
+    var player: MP3Player?
+    var isPlaying = false
+    var isSliding = false
+    @IBOutlet var trackTimeLabel: UILabel!
+    @IBOutlet var playOrPauseButton: UIButton!
+    @IBOutlet var progressSlider: UISlider!
+    
+    
+    
+    @IBAction func startSliding(sender: UISlider) {
+        print("start sliding")
+        isSliding = true
+    }
+   
+    @IBAction func endSliding(sender: UISlider) {
+        print("end sliding inside")
+        endEditingProgress(progressSlider.value)
+    }
+    
+    @IBAction func endSlidingOut(sender: UISlider) {
+        print("end sliding outside")
+        endEditingProgress(progressSlider.value)
+    }
+    
+    func endEditingProgress(newProgress: Float) {
+        isSliding = false
+        player?.setProgress(newProgress)
+        updateView()
+        switchPlay(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setMusicThumbImage()
+        trackTimeLabel.text = "00:00"
+        player = MP3Player()
+        startTimer()
+    }
+    
+    private func setMusicThumbImage() {
+        let tmpImage = UIImage(named: "musicSliderThumbImage")
+        let musicThumbImage = UIImage(data: UIImagePNGRepresentation(tmpImage!)!, scale: 1.4)
+        progressSlider.setThumbImage(musicThumbImage, forState: .Normal)
+    }
+
+    @IBAction func playButtonPressed(sender: UIButton) {
+        if isPlaying {
+            player?.playOrPause(false)
+        } else {
+            player?.playOrPause(true)
+        }
+        switchPlay(!isPlaying)
+    }
+    
+    @IBAction func nextButtonPressed(sender: UIButton) {
+        trackTimeLabel.text = "00:00"
+        switchPlay(true)
+        player?.next()
+    }
+    
+    @IBAction func preButtonPressed(sender: UIButton) {
+        trackTimeLabel.text = "00:00"
+        switchPlay(true)
+        player?.previous()
+    }
+    
+    @IBAction func returnSomeSeconds(sender: UIButton) {
+        switchPlay(true)
+        player?.returnWithSeconds(10)
+        updateView()
+    }
+    
+    @IBAction func forwardSomeSeconds(sender: UIButton) {
+        switchPlay(true)
+        player?.forwardWithSeconds(10)
+        updateView()
+    }
+    
+    func switchPlay (play: Bool) {
+        isPlaying = play
+        if play {
+            startTimer()
+            playOrPauseButton.setImage(UIImage(named: "pause_green"), forState: .Normal)
+        } else {
+            timer?.invalidate()
+            playOrPauseButton.setImage(UIImage(named: "play_green"), forState: .Normal)
+        }
+    }
+    
+    func startTimer() {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateView", userInfo: nil, repeats: true)
+    }
+    
+    func updateView() {
+        let minutes: Int = Int((player?.getCurrentTime())!) / 60
+        let seconds: Int = Int((player?.getCurrentTime())!) % 60
+        var timeString = ""
+        if minutes >= 10 {
+            timeString += "\(minutes)"
+        } else {
+            timeString += "0\(minutes)"
+        }
+        
+        if seconds >= 10 {
+            timeString += ":\(seconds)"
+        } else {
+            timeString += ":0\(seconds)"
+        }
+        
+        if isSliding == false {
+            progressSlider.value = (player?.getProgress())!
+        }
+        trackTimeLabel.text = timeString
+    }
+    
+}
